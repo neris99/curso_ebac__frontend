822a4ed5804d6cb9456c2c455dee818025af4538
Marcelo Neris <81583923+neris99@users.noreply.github.com>
2022-10-24 11:53:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const form = document.getElementById('form')
-const numA = parseFloat(document.getElementById('campoA').value)
-const numB = parseFloat(document.getElementById('campoB').value)
+const formulario = document.getElementById('#form')
+const numA = parseFloat(document.getElementById('#campoA'))
+const numB = parseFloat(document.getElementById('#campoB'))
 
 
-
-
-function validaForm(){
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault()
+    console.log(numA,numB)
     if(numA > numB){
         alert('O número do campo B precisa ser maior que o campo A, refaça')
     }else{
         alert('Parabéns')
     }
-}
+    
+})
+
+//function validaForm(){
+//    if(numA > numB){
+//        alert('O número do campo B precisa ser maior que o campo A, refaça')
+//    }else{
+//        alert('Parabéns')
+//    }
+//}
