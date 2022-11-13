b47708eafc1cbbf3829786b798c9736bdf268fae
Marcelo Neris <81583923+neris99@users.noreply.github.com>
2022-11-13 17:14:47 -0300

    ajuste no jquery

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ $(document).ready(function() {
     $('form').on('submit', function(e){
         e.preventDefault()
         const tarefa = $('#nova-tarefa').val()
-        const novaTarefa = $('<li></li>')
-        $(`${tarefa}`).appendTo(novaTarefa)
+        const novaTarefa = $(`<li>
+        ${tarefa}
+        </li>`)
         $(novaTarefa).appendTo('ul')
+        $('#nova-tarefa').val('')
     })
 })
