ea78a0228e09f48a0a0c0834be7948755049bbbd
Marcelo Neris <marceloaneris@gmail.com>
2023-04-02 11:21:53 -0300

    exercicio de funções em typescript

---
+++ src/exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+console.log(multiplicacao(5, 10));
+function saudacao(palavra1, palavra2) {
+    return palavra1 + ' ' + palavra2;
+}
+console.log(saudacao('Olá', 'Marcelo'));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,12 @@
+function multiplicacao(numero1: number, numero2: number){
+    return numero1 * numero2
+}
+
+console.log(multiplicacao(5, 10))
+
+
+function saudacao(palavra1: string, palavra2: string){
+    return palavra1 + ' ' + palavra2
+}
+
+console.log(saudacao('Olá', 'Marcelo'))
